d791ad481e916177d529667a0a86cbb939cf0db7
ProDeveloper <ramazani.david@gmail.com>
2022-02-05 19:35:55 +0330

    First commit

--- Api/src/server.js
+++ Api/src/server.js
@@ -4,8 +4,34 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+const withDB = async (operations, res) => {
+  try {
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+    });
+    const db = client.db("todolist-db");
+    await operations(db);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+};
+
+app.post("/api/v1/todos/create", async (req, res) => {
+  const { content } = req.body;
+
+  withDB(async (db) => {
+    const todo = await db.collection("todos").insertOne({
+      id: new ObjectId(),
+      content,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isCompleted: false,
+    });
+
+    console.log(todo);
+    res.status(201).json(todo);
+  }, res);
 });
 
 app.listen(3001, () => {
